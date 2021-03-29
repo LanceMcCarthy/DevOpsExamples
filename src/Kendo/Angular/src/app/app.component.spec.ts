33773a1c0af3893eb203c583c2e96f5998c0ce5e
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-03-29 15:44:33 -0400

    Update app.component.spec.ts
    Fixed Angular app tests

--- src/Kendo/Angular/src/app/app.component.spec.ts
+++ src/Kendo/Angular/src/app/app.component.spec.ts
@@ -20,16 +20,16 @@ describe('AppComponent', () => {
     expect(app).toBeTruthy();
   });
 
-  it(`should have as title 'Angular'`, () => {
+  it(`should have as title 'Kendo UI for Angular demo'`, () => {
     const fixture = TestBed.createComponent(AppComponent);
     const app = fixture.componentInstance;
-    expect(app.title).toEqual('Angular');
+    expect(app.title).toEqual('Kendo UI for Angular demo');
   });
 
   it('should render title', () => {
     const fixture = TestBed.createComponent(AppComponent);
     fixture.detectChanges();
     const compiled = fixture.nativeElement;
-    expect(compiled.querySelector('.content span').textContent).toContain('Angular app is running!');
+    expect(compiled.querySelector('.content span').textContent).toContain('Kendo UI for Angular demo app is running!');
   });
 });
