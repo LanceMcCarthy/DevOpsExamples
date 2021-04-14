f2bc72292a4a57072c712d177aec3eae8da2b374
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-04-14 19:09:34 -0400

    Delete appcenter-pre-build.ps1

--- src/Xamarin/appcenter-pre-build.ps1
+++
@@ -1,1 +0,0 @@
-nuget.exe sources Add -NonInteractive -Name TelerikNugetFeed -Source https://nuget.telerik.com/nuget  -UserName $env:TELERIK_USERNAME -Password $env:TELERIK_PASSWORD -StorePasswordInClearText
