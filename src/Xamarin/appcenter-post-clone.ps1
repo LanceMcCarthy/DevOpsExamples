f7cc8b4ca7ba0f5b6190e7e75843c0e034c12d66
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-04-14 19:24:59 -0400

    Remove UWP build from appcenter

--- src/Xamarin/appcenter-post-clone.ps1
+++
@@ -1,3 +0,0 @@
-Invoke-WebRequest https://dist.nuget.org/win-x86-commandline/latest/nuget.exe -OutFile nuget.exe
-
-nuget.exe sources Add -NonInteractive -Name TelerikNugetFeed -Source https://nuget.telerik.com/nuget  -UserName $env:TELERIK_USERNAME -Password $env:TELERIK_PASSWORD -StorePasswordInClearText
