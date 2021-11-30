5bc0e53e04f1b32b16211b9f3f6258c2ab654474
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2021-11-30 10:05:57 -0500

    Update appcenter-pre-build.sh

--- src/Xamarin/appcenter-pre-build.sh
+++ src/Xamarin/appcenter-pre-build.sh
@@ -1,3 +1,7 @@
 #!/usr/bin/env bash
 
-msbuild src/Xamarin/XamForms.sln /t:restore
+# Option 1 - Use msbuild restore before dotnet restore is used
+msbuild $APPCENTER_SOURCE_DIRECTORY/src/Xamarin/XamForms.sln /t:restore
+
+# Option 2 - Remove the UWP project form the solution before the build
+# dotnet sln $APPCENTER_SOURCE_DIRECTORY/src/Xamarin/XamForms.sln remove $APPCENTER_SOURCE_DIRECTORY/src/Xamarin/XamForms/UWP/XamForms.UWP.csproj
