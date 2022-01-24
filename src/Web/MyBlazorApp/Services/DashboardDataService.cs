bbd798d0e4374eefea6766aa895d6c4772c16d90
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-01-24 14:40:37 -0500

    Update Web project to R1 2022. Simplified demo to only a Grid.

--- src/Web/MyBlazorApp/Services/DashboardDataService.cs
+++ src/Web/MyBlazorApp/Services/DashboardDataService.cs
@@ -9,14 +9,12 @@ namespace MyBlazorApp.Services
     public class DashboardDataService
     {
         private static List<PodcastViewModel> podcasts = new List<PodcastViewModel>();
-        private Random random = new Random();
-        private List<string> firstNames = new List<string> { "Nancy", "Andrew", "Janet", "Margaret", "Steven", "Michael", "Robert", "Laura", "Anne", "Nige" };
-        private List<string> lastNames = new List<string> { "Davolio", "Fuller", "Leverling", "Peacock", "Buchanan", "Suyama", "King", "Callahan", "Dodsworth", "White" };
-        private List<string> platforms = new List<string> { "Apple Podcasts", "Spotify", "Other", "Overcast", "Anchor", "Stitcher" };
-        private List<string> devices = new List<string> { "iOS", "Android", "Other", "Web" };
-
-        // we generate some data and store it a static variable in this singleton service for simplicity
-        // in a real app you should use actaul database and real time data and replace this data retrieval logic
+        private readonly Random random = new Random();
+        private readonly List<string> firstNames = new List<string> { "Nancy", "Andrew", "Janet", "Margaret", "Steven", "Michael", "Robert", "Laura", "Anne", "Nige" };
+        private readonly List<string> lastNames = new List<string> { "Davolio", "Fuller", "Leverling", "Peacock", "Buchanan", "Suyama", "King", "Callahan", "Dodsworth", "White" };
+        private readonly List<string> platforms = new List<string> { "Apple Podcasts", "Spotify", "Other", "Overcast", "Anchor", "Stitcher" };
+        private readonly List<string> devices = new List<string> { "iOS", "Android", "Other", "Web" };
+        
         public DashboardDataService()
         {
             if (podcasts.Count == 0)
@@ -33,8 +31,7 @@ namespace MyBlazorApp.Services
                 }).ToList();
             }
         }
-
-        // methods from the service that are used across the components to get data
+        
         public async Task<IEnumerable<PodcastViewModel>> GetPodcasts()
         {
             return await Task.FromResult(podcasts);
@@ -42,19 +39,19 @@ namespace MyBlazorApp.Services
 
         public async Task<int> GetStreams()
         {
-            int result = podcasts.Sum(f => f.Streams);
+            var result = podcasts.Sum(f => f.Streams);
             return await Task.FromResult(result);
         }
 
         public async Task<int> GetDownloads()
         {
-            int result = podcasts.Sum(f => f.Downloads);
+            var result = podcasts.Sum(f => f.Downloads);
             return await Task.FromResult(result);
         }
 
         public async Task<int> GetReach()
         {
-            int result = podcasts.Sum(f => f.Reach);
+            var result = podcasts.Sum(f => f.Reach);
             return await Task.FromResult(result);
         }
 
@@ -66,12 +63,13 @@ namespace MyBlazorApp.Services
 
         public async Task<IEnumerable<PlatformViewModel>> GetPlatformData(bool byDevice)
         {
-            var deviceViews = podcasts.GroupBy(x => byDevice == true ? x.Device : x.PlatformName)
-                                .Select(x => new PlatformViewModel
-                                {
-                                    Category = x.Key,
-                                    Views = x.Sum(v => v.Views)
-                                });
+            var deviceViews = podcasts
+                .GroupBy(x => byDevice == true ? x.Device : x.PlatformName)
+                .Select(x => new PlatformViewModel
+                {
+                    Category = x.Key,
+                    Views = x.Sum(v => v.Views)
+                });
 
             return await Task.FromResult(deviceViews);
         }
