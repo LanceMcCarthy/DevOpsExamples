6e52745df47a75ba9adf7158d574f8797bb75de0
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-10-12 12:15:12 -0400

    [ASP.NET Core] Split apart .NET web projects
    This is a workflow-breaking change. I split apart the combined Blazor and ASP.NET Core projects into separate solutions, folders and workflows

---
+++ src/AspNetCore/MyAspNetCoreApp/Controllers/DashboardController.cs
@@ -0,0 +1,75 @@
+﻿using Kendo.Mvc.Extensions;
+using Kendo.Mvc.UI;
+using Microsoft.AspNetCore.Mvc;
+using MyAspNetCoreApp.Models;
+
+namespace MyAspNetCoreApp.Controllers
+{
+    public class DashboardController : Controller
+    {
+        private readonly List<string> firstNames = new() { "Nancy", "Andrew", "Janet", "Margaret", "Steven", "Michael", "Robert", "Laura", "Anne", "Nige" };
+        private readonly List<string> lastNames = new() { "Davolio", "Fuller", "Leverling", "Peacock", "Buchanan", "Suyama", "King", "Callahan", "Dodsworth", "White" };
+        private readonly List<string> platforms = new() { "Apple Podcasts", "Spotify", "Other", "Overcast", "Anchor", "Stitcher" };
+        private readonly List<string> devices = new() { "iOS", "Android", "Other", "Web" };
+
+        public static Random Random = new();
+
+        public static List<PodcastViewModel> Podcasts = new();
+
+        public ActionResult Podcasts_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var result = GetPodcasts().ToDataSourceResult(request);
+            ViewData["test"] = result.AggregateResults;
+            return Json(result);
+        }
+
+        public ActionResult Downloads_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            return Json(GetPodcasts());
+        }
+
+        public ActionResult Devices_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var deviceViews = GetPodcasts()
+                .GroupBy(x => x.Device)
+                .Select(x => new
+                {
+                    Device = x.Key,
+                    Views = x.Sum(v => v.Views)
+                });
+
+            return Json(deviceViews);
+        }
+
+        public ActionResult Platforms_Read([DataSourceRequest] DataSourceRequest request)
+        {
+            var platformViews = GetPodcasts()
+                .GroupBy(x => x.PlatformName)
+                .Select(x => new
+                {
+                    PlatformName = x.Key,
+                    Views = x.Sum(v => v.Views)
+                });
+
+            return Json(platformViews);
+        }
+        private IEnumerable<PodcastViewModel> GetPodcasts()
+        {
+            if (Podcasts.Count == 0)
+            {
+                Podcasts = Enumerable.Range(1, 50).Select(x => new PodcastViewModel()
+                {
+                    Name = $"Episode #{Random.Next(0, 200)} with guest {firstNames[Random.Next(0, firstNames.Count)]} {lastNames[Random.Next(0, lastNames.Count)]}",
+                    Streams = Random.Next(0, 18000),
+                    Downloads = Random.Next(0, 15000),
+                    PlatformName = platforms[Random.Next(0, platforms.Count)],
+                    Device = devices[Random.Next(0, devices.Count)],
+                    Date = DateTime.Now.AddDays(-x),
+                    Reach = x * Random.Next(0, 1000),
+                }).ToList();
+            }
+
+            return Podcasts;
+        }
+    }
+}
---
+++ src/AspNetCore/MyAspNetCoreApp/Models/PodcastViewModel.cs
@@ -0,0 +1,27 @@
+﻿namespace MyAspNetCoreApp.Models
+{
+    public class PodcastViewModel
+    {
+        private int views;
+
+        public string? Name { get; set; }
+
+        public int Downloads { get; set; }
+
+        public int Streams { get; set; }
+
+        public int Views
+        {
+            get => Downloads + Streams;
+            private set => views = value;
+        }
+
+        public DateTime Date { get; set; }
+
+        public int Reach { get; set; }
+
+        public string? Device { get; set; }
+
+        public string? PlatformName { get; set; }
+    }
+}
