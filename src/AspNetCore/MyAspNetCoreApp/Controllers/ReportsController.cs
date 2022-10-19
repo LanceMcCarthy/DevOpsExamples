2066ea88a0a8ad6f6157d3c716af159d862b638f
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2022-10-19 13:40:19 -0600

    Add Reporting REST service with Linux support to ASP.NET Core demo (#94)
    * Update Dockerfile - Added Linux dependencies for REST service Report rendering
    * [aspnetcore] Added Reporting REST service

---
+++ src/AspNetCore/MyAspNetCoreApp/Controllers/ReportsController.cs
@@ -0,0 +1,30 @@
+﻿using Microsoft.AspNetCore.Mvc;
+using System.Net;
+using System.Net.Mail;
+using Telerik.Reporting.Services;
+using Telerik.Reporting.Services.AspNetCore;
+
+namespace MyAspNetCoreApp.Controllers;
+
+[Route("api/reports")]
+[ApiController]
+public class ReportsController : ReportsControllerBase
+{
+    public ReportsController(IReportServiceConfiguration reportServiceConfiguration)
+        : base(reportServiceConfiguration)
+    {
+    }
+
+    protected override HttpStatusCode SendMailMessage(MailMessage mailMessage)
+    {
+        throw new System.NotImplementedException("This method should be implemented in order to send mail messages");
+
+        // using (var smtpClient = new SmtpClient("smtp01.mycompany.com", 25))
+        // {
+        //     smtpClient.DeliveryMethod = SmtpDeliveryMethod.Network;
+        //     smtpClient.EnableSsl = false;
+        //     smtpClient.Send(mailMessage);
+        // }
+        // return HttpStatusCode.OK;
+    }
+}
--- src/AspNetCore/MyAspNetCoreApp/Program.cs
+++ src/AspNetCore/MyAspNetCoreApp/Program.cs
@@ -1,12 +1,32 @@
-﻿var builder = WebApplication.CreateBuilder(args);
+﻿using Microsoft.Extensions.DependencyInjection.Extensions;
+using Telerik.Reporting.Cache.File;
+using Telerik.Reporting.Services;
+
+var builder = WebApplication.CreateBuilder(args);
+
+
+builder.Services.AddCors(corsOption => corsOption
+    // CORS policy for ReportsController
+    .AddPolicy("ReportingRestPolicy", corsBuilder => corsBuilder.AllowAnyOrigin().AllowAnyMethod().AllowAnyHeader()
+));
 
 // Add services to the container.
 builder.Services.AddControllersWithViews()
-                // Maintain property names during serialization. See:
-                // https://github.com/aspnet/Announcements/issues/194
                 .AddNewtonsoftJson(options => options.SerializerSettings.ContractResolver = new Newtonsoft.Json.Serialization.DefaultContractResolver());
+
 // Add Kendo UI services to the services container"
 builder.Services.AddKendo();
+
+// Configure dependencies for ReportsController.
+builder.Services.TryAddSingleton<IReportServiceConfiguration>(sp =>
+    new ReportServiceConfiguration
+    {
+        ReportingEngineConfiguration = sp.GetService<IConfiguration>(),
+        HostAppId = "MyAspNetCoreApp",
+        Storage = new FileStorage(),
+        ReportSourceResolver = new UriReportSourceResolver(System.IO.Path.Combine(sp.GetService<IWebHostEnvironment>().ContentRootPath, "Reports"))
+    });
+
 
 var app = builder.Build();
 
@@ -20,13 +40,10 @@ if (!app.Environment.IsDevelopment())
 
 app.UseHttpsRedirection();
 app.UseStaticFiles();
-
 app.UseRouting();
-
+app.UseCors("ReportingRestPolicy");
 app.UseAuthorization();
-
-app.MapControllerRoute(
-    name: "default",
-    pattern: "{controller=Home}/{action=Index}/{id?}");
+app.MapControllers();
+app.MapControllerRoute(name: "default", pattern: "{controller=Home}/{action=Index}/{id?}");
 
 app.Run();
