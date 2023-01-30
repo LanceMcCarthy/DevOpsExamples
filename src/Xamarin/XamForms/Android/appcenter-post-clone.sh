ab1ab455a9e27126cd42bdb5701ee95562ca85e6
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-01-30 17:01:13 -0500

    [appcenter] Add NET 7 install to post clone scripts

--- src/Xamarin/XamForms/Android/appcenter-post-clone.sh
+++ src/Xamarin/XamForms/Android/appcenter-post-clone.sh
@@ -1,2 +1,8 @@
 #!/usr/bin/env bash
 dotnet sln "$APPCENTER_SOURCE_DIRECTORY/src/Xamarin/XamForms.sln" remove "$APPCENTER_SOURCE_DIRECTORY/src/Xamarin/XamForms/UWP/XamForms.UWP.csproj"
+
+wget -O dotnet-install.sh https://dot.net/v1/dotnet-install.sh
+
+sudo chmod +x ./dotnet-install.sh
+
+./dotnet-install.sh --version latest
