90ea8d4ddaa138a219cad62caf1c5accad9d81f4
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-04-18 18:30:58 -0700

    Add new Kendo Angular project

---
+++ src/Kendo/angular_demo/angular_demo/src/app/app-routing.module.ts
@@ -0,0 +1,16 @@
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+
+import { HomeComponent } from './components/home/home.component';
+
+const routes: Routes = [
+  { path: '', redirectTo: 'home', pathMatch: 'full' },
+  { path: 'home', component: HomeComponent },
+  { path: '**', redirectTo: 'home' }
+];
+
+@NgModule({
+  imports: [RouterModule.forRoot(routes)],
+  exports: [RouterModule]
+})
+export class AppRoutingModule { }
---
+++ src/Kendo/angular_demo/angular_demo/src/app/app.module.ts
@@ -0,0 +1,48 @@
+import { BrowserModule } from '@angular/platform-browser';
+import { NgModule } from '@angular/core';
+import { RouterModule, Routes } from '@angular/router';
+import { BrowserAnimationsModule } from '@angular/platform-browser/animations';
+import { ReactiveFormsModule, FormsModule } from '@angular/forms';
+import { AppComponent } from './app.component';
+import { MenuModule } from '@progress/kendo-angular-menu';
+import { GridModule } from '@progress/kendo-angular-grid';
+import { ChartsModule } from '@progress/kendo-angular-charts';
+import { DropDownsModule } from '@progress/kendo-angular-dropdowns';
+import { PopupModule } from '@progress/kendo-angular-popup';
+import { InputsModule } from '@progress/kendo-angular-inputs';
+import { LabelModule } from '@progress/kendo-angular-label';
+import { ButtonsModule } from '@progress/kendo-angular-buttons';
+
+import 'hammerjs';
+
+import { AppRoutingModule } from './app-routing.module';
+
+import { HeaderComponent } from './components/header/header.component';
+import { HomeComponent } from './components/home/home.component';
+import { FooterComponent } from './components/footer/footer.component';
+
+@NgModule({
+    declarations: [
+        AppComponent,
+        HeaderComponent,
+        HomeComponent,
+        FooterComponent
+    ],
+    imports: [
+      BrowserModule,
+      ReactiveFormsModule,
+      FormsModule,
+      MenuModule,
+      BrowserAnimationsModule,
+      GridModule,
+      ChartsModule,
+      AppRoutingModule,
+      DropDownsModule,
+      PopupModule,
+      InputsModule,
+      LabelModule,
+      ButtonsModule
+  ],
+    bootstrap: [AppComponent]
+})
+export class AppModule { }
