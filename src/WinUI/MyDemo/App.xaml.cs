b159f26a516ce3b107aad8495e67b4fc0911560f
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2023-09-01 16:48:39 -0400

    Add WinUI 3 demo and GitHub Actions workflow (#121)
    * Add WinUI3 test project for CI build validation
    * Create main_build-winui.yml

---
+++ src/WinUI/MyDemo/App.xaml.cs
@@ -0,0 +1,21 @@
+﻿using WinUIEx;
+
+namespace MyDemo;
+
+public partial class App : Microsoft.UI.Xaml.Application
+{
+    public App()
+    {
+        this.InitializeComponent();
+    }
+
+    protected override void OnLaunched(Microsoft.UI.Xaml.LaunchActivatedEventArgs args)
+    {
+        m_window = new MainWindow();
+        m_window.Activate();
+
+        m_window.CenterOnScreen(1024,768);
+    }
+
+    private Microsoft.UI.Xaml.Window m_window;
+}
---
+++ src/WinUI/MyDemo/MainWindow.xaml.cs
@@ -0,0 +1,11 @@
+using Microsoft.UI.Xaml;
+
+namespace MyDemo;
+
+public sealed partial class MainWindow : Window
+{
+    public MainWindow()
+    {
+        InitializeComponent();
+    }
+}
