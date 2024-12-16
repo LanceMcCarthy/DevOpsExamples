8ba4e36ea6dd8b7b138379750fda9853acc949ca
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2024-12-16 17:18:36 -0500

    [maui] Removed 8.0.0 theming

--- src/MAUI/App.xaml.cs
+++ src/MAUI/App.xaml.cs
@@ -6,23 +6,26 @@ public partial class App : Application
 	{
 		InitializeComponent();
 
-        Application.Current.UserAppTheme = AppTheme.Unspecified;
-        Application.Current.RequestedThemeChanged += (s, e) => ApplyTelerikTheme();
+        this.UserAppTheme = AppTheme.Unspecified;
+        this.RequestedThemeChanged += (s, e) => ApplyTelerikTheme();
         this.ApplyTelerikTheme();
+    }
 
-		MainPage = new AppShell();
-	}
+    protected override Window CreateWindow(IActivationState activationState)
+    {
+        if (this.MainPage == null)
+        {
+            this.MainPage = activationState?.Context.Services.GetRequiredService<AppShell>();
+        }
+
+        return base.CreateWindow(activationState);
+    }
 
     private void ApplyTelerikTheme()
     {
-        var telerikTheming = Application.Current
-            .Resources
-            .MergedDictionaries
-            .OfType<TelerikTheming>()
-            .Single();
+        var swatchName = this.RequestedTheme == AppTheme.Dark ? "Purple Dark" : "Purple";
 
-        var swatchName = Application.Current.RequestedTheme == AppTheme.Dark ? "Purple Dark" : "Purple";
-        telerikTheming.Theme = TelerikTheming.Themes
-            .Single(t => t.Theme == "Telerik" && t.Swatch == swatchName);
+        this.Resources.MergedDictionaries.OfType<TelerikTheming>()
+            .Single().Theme = TelerikTheming.Themes.Single(t => t.Theme == "Telerik" && t.Swatch == swatchName);
     }
 }
