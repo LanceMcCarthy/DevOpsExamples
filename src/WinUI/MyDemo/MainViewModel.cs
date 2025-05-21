efd217a6a74e9b72a0133ecd9bf3630792ef0c21
Lance McCarthy <LanceMcCarthy@users.noreply.github.com>
2025-05-21 16:59:30 -0400

    WinUI - Fixes and updates

--- src/WinUI/MyDemo/MainViewModel.cs
+++ src/WinUI/MyDemo/MainViewModel.cs
@@ -1,25 +1,25 @@
 using System;
+using System.Collections.ObjectModel;
+using CommonHelpers.Common;
 using CommonHelpers.Models;
-using Telerik.Core;
 
 namespace MyDemo;
 
-public class MainViewModel : CommonHelpers.Common.ViewModelBase
+public class MainViewModel : ViewModelBase
 {
     public MainViewModel()
     {
+        Employees =
+        [
+            new() { Name = "John Doe", Salary = 10000, StartDate = DateTime.Today, Position = "TSE I", VacationTotal = 21, VacationUsed = 7 },
+            new() { Name = "John Doe Junior", Salary = 10000, StartDate = DateTime.Today, Position = "TSE II", VacationTotal = 21, VacationUsed = 7 },
+            new() { Name = "Jane Doe", Salary = 10000, StartDate = DateTime.Today, Position = "TSE IV", VacationTotal = 21, VacationUsed = 7 }
+        ];
     }
 
-    private BindableCollection<Employee> employees = new()
+    public ObservableCollection<Employee> Employees
     {
-        new() { Name = "John Doe", Salary = 10000, StartDate = DateTime.Today, Position = "TSE I", VacationTotal = 21, VacationUsed = 7 },
-        new() { Name = "John Doe Junior", Salary = 10000, StartDate = DateTime.Today, Position = "TSE II", VacationTotal = 21, VacationUsed = 7 },
-        new() { Name = "Jane Doe", Salary = 10000, StartDate = DateTime.Today, Position = "TSE IV", VacationTotal = 21, VacationUsed = 7 },
-    };
-
-    public BindableCollection<Employee> Employees
-    {
-        get => employees;
-        set => SetProperty(ref employees, value);
+        get;
+        set => SetProperty(ref field, value);
     }
 }
